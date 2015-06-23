198742a71d5f6f74a7dcf73c4af06e2cb7c49666
marcemonterot <marce@marce-portatil.marce.com>
2015-06-23 21:43:24 +0200

    Añadirr favicon y quitar el router users

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//importamos todos los modulos necesarios
+//para la aplicacion MVC
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,26 +7,36 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//importamos los enrutaores
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+//renderizador de las vistas importante al
+//hacer la generacion de la estructura con
+//express-generator poner este parametro
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+//se descomenta para montar el middleware del favicon
+app.use(favicon(__dirname + '/public/favicon.ico'));
+//instalar los middlewares predefinidos
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//instalar los enrutadores a la pagina de
+//index y a la de users. tambien se puede instalar con get
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
+//se ejecutara para cualquier ruta que
+//no entre por las anteriores de rutas
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -36,6 +48,8 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
+//se instalara este middleware solo en el
+//entorno de desarrollo, si no estamos ahi no se instalara
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -48,6 +62,8 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
+//se instalara este middleware para
+//el resto de entornos
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -56,5 +72,5 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
+//exportar para el comando de arranque
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+//se debe volver a cargar ota vez en este modulos
 var express = require('express');
+//generador de ruta
 var router = express.Router();
 
 /* GET home page. */
+//renderiaza el index.ejs
+//le cambia el <%= title %> por 'Express'
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
 
 module.exports = router;
