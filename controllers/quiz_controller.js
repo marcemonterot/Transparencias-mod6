77de03fe7cd6f58ff1a39fccfad2e7f8ae9ee1c7
marcemonterot <marce@marce-portatil.marce.com>
2015-06-23 22:31:32 +0200

    Primera pagina

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,18 @@
+exports.question=function(req,res)
+{
+  res.render('quizes/question',{Titulo:'Capital de Italia', title:'Pregunta'});
+}
+
+
+exports.answer=function(req,res)
+{
+  aux=req.query.respuesta.match(/roma/i);
+  if (aux==null)
+  {
+    res.render('quizes/answer',{Resultado:'Incorrecta', title:'Respuesta', respuestaUsu:req.query.respuesta});
+  }
+  else {
+    res.render('quizes/answer',{Resultado:'Correcta', title:'Respuesta', respuestaUsu:req.query.respuesta});
+  }
+
+}
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,20 @@ var express = require('express');
 //generador de ruta
 var router = express.Router();
 
+//importo el quiz controller en el index que es desde donde se gestionara
+var quizController=require('../controllers/quiz_controller.js')
+
 /* GET home page. */
 //renderiaza el index.ejs
-//le cambia el <%= title %> por 'Express'
+//le cambia el <%= title %> por 'Quiz'
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+//get para la pregunta
+router.get('/quizes/question', quizController.question);
+
+//get para la respuesta
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
