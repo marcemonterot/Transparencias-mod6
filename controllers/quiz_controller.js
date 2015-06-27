60cf6894055a6bb7734c19dee9abfb0eae6a8ea3
marcemonterot <marce@marce-portatil.marce.com>
2015-06-27 16:48:45 +0200

    añadido de BD
    config postgres Heroku y sqlite en local

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,20 +1,27 @@
 //toda accion de un controlador que responda a un GET tiene una vista asociada
+var models=require('../models/models.js');
 
 exports.question=function(req,res)
 {
-  res.render('quizes/question',{Titulo:'Capital de Italia', title:'Pregunta'});
+  models.Quiz.findAll().success(function(quiz){
+    res.render('quizes/question',{title:'Pregunta', Titulo:quiz[0].pregunta});
+  });
+
 }
 
 
 exports.answer=function(req,res)
 {
-  aux=req.query.respuesta.match(/roma/i);
-  if (aux==null)
-  {
-    res.render('quizes/answer',{Resultado:'Incorrecta', title:'Respuesta', respuestaUsu:req.query.respuesta});
-  }
-  else {
-    res.render('quizes/answer',{Resultado:'Correcta', title:'Respuesta', respuestaUsu:req.query.respuesta});
-  }
-
+  models.Quiz.findAll().success(function(quiz){
+    //meto la expresion regular el case insensitive
+    //var re = new RegExp(quiz[0].respuesta,"i");
+    //aux=req.query.respuesta.toLowerCase();
+    if (req.query.respuesta.toLowerCase()!=quiz[0].respuesta.toLowerCase())
+    {
+      res.render('quizes/answer',{Resultado:'Incorrecta', title:'Respuesta', respuestaUsu:req.query.respuesta});
+    }
+    else {
+      res.render('quizes/answer',{Resultado:'Correcta', title:'Respuesta', respuestaUsu:req.query.respuesta});
+    }
+  });
 }
---
+++ models/models.js
@@ -0,0 +1,52 @@
+var path=require('path');
+
+//preparo los parametros de conexion de postgres o de sqlite
+//DATABASE_URL puede ser lo del .env o la de conf de postgres en heroku
+var url=process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name=(url[6]||null);
+var user=(url[2]||null);
+var pwd=(url[3]||null);
+var protocol=(url[1]||null);
+var dialect=(url[1]||null);
+var port=(url[5]||null);
+var host=(url[4]||null);
+var storage=process.env.DATABASE_STORAGE;
+
+//cargo sequelize (el orm: object relational mapping)
+var Sequelize=require('sequelize');
+
+//configuracion de Sequelize para usar SQLite
+var sequelize = new Sequelize (DB_name,user,pwd,
+                              {
+                               dialect:protocol,
+                               protocol:protocol,
+                               port:port,
+                               host:host,
+                               storage:storage,
+                               omitNull: true //solo postgres
+                               }
+);
+
+//importo la definicion del modelo quiz.js, la tabla QUIZ
+var Quiz=sequelize.import(path.join(__dirname,'quiz'));
+
+//exporto la definicion para que la puedan usar los controladores
+//actuar sobre la tabla QUIZ
+exports.Quiz=Quiz;
+
+//sincronizo la definicion del modelo con la base de datos quiz.sqlite
+//por si no esta creada la tabla o se ha actualizado
+sequelize.sync().success(function(){
+    Quiz.count().success(function(count){
+      console.log(count);
+      if (count===0){
+        //inserta una nueva fila
+        Quiz.create({
+                      pregunta:'Capital de Italia',
+                      respuesta:'Roma'
+                    }).success(function(){
+                      console.log('tabla creada e inicializada')
+                      });
+      };
+    });
+});
