e27609d01071c24d5999516312389dd8c797bd14
marcemonterot <marce@marce-portatil.marce.com>
2015-06-27 19:27:34 +0200

    Cambios autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,27 +1,73 @@
 //toda accion de un controlador que responda a un GET tiene una vista asociada
 var models=require('../models/models.js');
 
+//lo primero de todo creo la funcion que cargará el objeto (autoload)
+//asi se simplificará el codigo y meteremos el tratamiento de errores
+//unficado
+exports.load=function(req,res,next,idpregunta){
+  models.Quiz.find(idpregunta).then(function(quiz){
+    if (quiz)
+    {
+      req.quiz=quiz;
+      next();
+    }
+    else {
+      next(new Error('La pregunta '+idpregunta+' no existe'));
+    }
+  }).catch(function(error){next(error)});
+}
+
+//esta funcion era para recuperar una pregunta cuando solo teniamos
+//una en base de datos, se dejará de usar
 exports.question=function(req,res)
 {
   models.Quiz.findAll().success(function(quiz){
     res.render('quizes/question',{title:'Pregunta', Titulo:quiz[0].pregunta});
   });
-
 }
 
+//esta funcion me mostrará todas las preguntas de la tabla preguntas base de datos
+exports.index=function(req,res)
+{
+    models.Quiz.findAll().then(function(quizes){
+    res.render('quizes/index',{quizes:quizes});
+  });
+}
+
+//esta funcion me mostrar la pregunta recuperada que se le pasa en la ur
+//de la API REST
+exports.show=function(req,res)
+{
+  //el find me devuelve el objeto de base de datos
+  //ya no hace falta por que hemos metido el autoload
+  //models.Quiz.find(req.params.idpregunta).then(function(quiz){
+    res.render('quizes/show',{quiz:req.quiz})
+  //});
+}
 
 exports.answer=function(req,res)
 {
-  models.Quiz.findAll().success(function(quiz){
+  //se cambia para que coja la pregunta que se le pasa por parametro
+  //models.Quiz.findAll().success(function(quiz){
+    //ya no hace falta por que hemos metido el autoload
+  //models.Quiz.find(req.params.idpregunta).then(function(quiz){
     //meto la expresion regular el case insensitive
     //var re = new RegExp(quiz[0].respuesta,"i");
     //aux=req.query.respuesta.toLowerCase();
-    if (req.query.respuesta.toLowerCase()!=quiz[0].respuesta.toLowerCase())
+    if (req.query.respuesta.toLowerCase()!=req.quiz.respuesta.toLowerCase())
     {
-      res.render('quizes/answer',{Resultado:'Incorrecta', title:'Respuesta', respuestaUsu:req.query.respuesta});
+      res.render('quizes/answer',{
+                              Resultado:'Incorrecta',
+                              title:'Respuesta',
+                              respuestaUsu:req.query.respuesta,
+                              quiz:req.quiz});
     }
     else {
-      res.render('quizes/answer',{Resultado:'Correcta', title:'Respuesta', respuestaUsu:req.query.respuesta});
-    }
-  });
+      res.render('quizes/answer',{
+                              Resultado:'Correcta',
+                              title:'Respuesta',
+                              respuestaUsu:req.query.respuesta,
+                              quiz:req.quiz});
+    };
+//  });
 }
--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,10 @@ var authorsController=require('../controllers/autores_controller.js')
 //renderiaza el index.ejs
 //le cambia el <%= title %> por 'Quiz'
 
+//si el parametro existe en la ruta ejecuta el controlador
+//se debe instalar antes de los middleware que hacen los get
+router.param('idpregunta',quizController.load);
+
 //toda ruta definida en el routes tiene una accion asociada en algun controlador
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
@@ -19,8 +23,16 @@ router.get('/', function(req, res) {
 //get para la pregunta, accion asociada del controlador quizController es question
 router.get('/quizes/question', quizController.question);
 
+//usamos este middleware para listar todas las preguntas
+router.get('/quizes/', quizController.index);
+
+//usamos este middleware para listar todas las preguntas
+router.get('/quizes/:idpregunta(\\d+)', quizController.show);
+
 //get para la respuesta
-router.get('/quizes/answer', quizController.answer);
+//router.get('/quizes/answer', quizController.answer);
+//modificamos el middleware para que coja la respuesta a la pregunta id
+router.get('/quizes/:idpregunta(\\d+)/answer', quizController.answer);
 
 //get para os autores
 router.get('/author', authorsController.autores);
